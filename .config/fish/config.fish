e3c70feaca623d6a9ea9f14b8efa9a44d53bcdcc
goldimax <goldimax1688@gmail.com>
2017-08-26 23:47:26 +0800

    fish alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,11 @@ end
 function fish_right_prompt
   env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/.shell_prompt.sh right
 end
+
+alias 'gdb'='gdb -q'
+alias 'cb'='xclip -selection clipboard'
+
+function cxx
+  clang++ --std=c++11 $argv
+end
+
